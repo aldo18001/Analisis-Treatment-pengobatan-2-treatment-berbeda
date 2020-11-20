fc9f22a6fde37c958799e49f03054e08dc07ec24
Aldo <aldo18001@mail.unpad.ac.id>
2020-11-20 19:30:13 +0700

    modifikasi tabel dan upload tabel

--- Persiapan Data.R
+++ Persiapan Data.R
@@ -1,10 +1,24 @@
 library(readxl)
 data <- read_excel("data.xls")
-View(data)
+head(data)
+
+data$Treat <- as.factor(data$Treat)
+data <- data[,-1]
+head(data)
+
 
 #Memisahkan data treatment A dan treatment AB
 
-data_A <- data[which(data$Treat == 'A'),]
-data_B <- data[which(data$Treat == 'AB'),]
+data_A <- data[which(data$Treat == 'A'),-1:-2]
+data_AB <- data[which(data$Treat == 'AB'),-1:-2]
 head(data_A)
-head(data_B)
+head(data_AB)
+
+
+#Modifikasi Tabel
+diff_data <- data.frame("Treat" = data$Treat , "week5" = data$QoL1-data$QoL0, "week10" = data$QoL2-data$QoL1, 
+                        "week15" = data$QoL3-data$QoL2)
+head(diff_data)
+
+library(writexl)
+write_xlsx(diff_data, "tabel selisih.xlsx")
