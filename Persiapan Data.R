e035c7817181b57676422c64b8fdd8f6c8815951
Aldo <aldo18001@mail.unpad.ac.id>
2020-11-20 17:53:29 +0700

    update pemisahan data

--- Persiapan Data.R
+++ Persiapan Data.R
@@ -1,3 +1,10 @@
 library(readxl)
 data <- read_excel("data.xls")
 View(data)
+
+#Memisahkan data treatment A dan treatment AB
+
+data_A <- data[which(data$Treat == 'A'),]
+data_B <- data[which(data$Treat == 'AB'),]
+head(data_A)
+head(data_B)
